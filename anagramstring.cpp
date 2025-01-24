518d234ead4f05760d5d97218ef74e8d8265d6c6
Nitesh Meena <165362413+ItsmeNiteshMeena@users.noreply.github.com>
2025-01-24 10:28:12 +0530

    Add files via upload
    C++ PROGRAM TO CHECK THE GIVEN TWO STRINGS ARE ANAGRAM OR NOT

---
+++ anagramstring.cpp
@@ -0,0 +1,26 @@
+//WAP TO CHECK GIVEN STRINGS ARE ANAGRAM OR NOT 
+
+// #include <iostream>
+// #include <string>
+// #include <algorithm>
+// using namespace std;
+// int main()
+// {
+//     string s="god";
+//     string t="dog";
+//     bool c=false;
+//     if(s.size()!=t.size())
+//     {
+//         cout<<"not anagram";
+//         return 0;
+//     }
+//    sort(s.begin(),s.end());
+//    sort(t.begin(),t.end());
+//    if(s==t)
+//    {
+//     cout<<"anagram ";
+//    }
+//    else{
+//     cout<<"not anagram ";
+//    }
+//}
